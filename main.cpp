14f1e439961374a29222d5750fbd5420d0dea40b
David Elmo Ross <david.elmo.ross@icloud.com>
2022-10-13 20:46:59 -0400

    added stuff
    added stuff

---
+++ main.cpp
@@ -0,0 +1,21 @@
+/*
+    Title:  SKELETON CODE for LCD, 4 digit Display, and Bluefruit Uart Friend
+    Author: YOUR NAME HERE
+    Date:   Today's Date HERE
+    Description:    
+*/
+
+// INCLUDE SECTION
+#include "mbed.h"                           // include the mbed library
+#include "Grove_LCD_RGB_Backlight.h"        // include LCD library
+#include "DigitDisplay.h"                   // 4 digit display library
+
+//  HARDWARE INTERFACE SECTION
+Grove_LCD_RGB_Backlight rgbLCD(D14, D15);   // pins for LCD connection(D14,D15)
+DigitDisplay segment(D2,D3);                // 4 digit display connected to D16
+Serial blue(D1,D0);                         // blue is of class serial (D1,D0) 
+                                            // associated with UART
+int main()
+{
+   
+}
